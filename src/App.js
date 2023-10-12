7661e97b0364f857f904c6d8c1e22583749ce015
Anirban Banerjee <anirban.banerjee2021@gmail.com>
2023-10-13 00:50:27 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react';
+import AgeCalc from './components/AgeCalc';
+
+function App() {
+  return (
+    <div className="App">
+      <AgeCalc/>
+    </div>
+  );
+}
+
+export default App;
